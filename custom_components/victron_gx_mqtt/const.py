984357ef02f0ac92fecd83e846a62f5f8dc50988
GreenHomeEnergy <116529059+GreenHomeEnergy@users.noreply.github.com>
2026-01-17 00:37:53 +0100

    Update const.py

--- custom_components/victron_gx_mqtt/const.py
+++ custom_components/victron_gx_mqtt/const.py
@@ -1,3 +1,18 @@
 from __future__ import annotations
 
+from homeassistant.const import Platform
+
 DOMAIN = "victron_gx_mqtt"
+
+PLATFORMS: list[Platform] = [Platform.SENSOR]
+
+CONF_NAME = "name"
+CONF_TOPIC_PREFIX = "topic_prefix"
+CONF_PORTAL_ID = "portal_id"
+
+CONF_SELECTED_SERVICES = "selected_services"
+
+SERVICE_VEBUS = "vebus"
+SERVICE_SYSTEM = "system"
+SERVICE_SOLARCHARGER = "solarcharger"
+SERVICE_BATTERY = "battery"
