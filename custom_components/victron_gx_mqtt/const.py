635fa69e3bded3c91140cd536b26be52e5f34a04
GreenHomeEnergy <116529059+GreenHomeEnergy@users.noreply.github.com>
2026-01-17 13:04:09 +0100

    Update const.py

--- custom_components/victron_gx_mqtt/const.py
+++ custom_components/victron_gx_mqtt/const.py
@@ -29,18 +29,19 @@ VE_BUS_STATE_MAP: Final[dict[int, str]] = {
 }
 
 # VE.Bus Mode mapping (writeable)
+# Vorgabe: 1=Charger Only; 2=Inverter Only; 3=On; 4=Off
 VE_BUS_MODE_MAP: Final[dict[int, str]] = {
-    0: "Off",
     1: "Charger Only",
     2: "Inverter Only",
     3: "On",
+    4: "Off",
 }
 
 VE_BUS_MODE_OPTIONS: Final[list[str]] = [
-    VE_BUS_MODE_MAP[0],
     VE_BUS_MODE_MAP[1],
     VE_BUS_MODE_MAP[2],
     VE_BUS_MODE_MAP[3],
+    VE_BUS_MODE_MAP[4],
 ]
 
 VE_BUS_MODE_MAP_INV: Final[dict[str, int]] = {v: k for k, v in VE_BUS_MODE_MAP.items()}
